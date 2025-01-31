893346972b625145089510d60b32d135bee82ec8
Vovan <vovagor1234321@gmail.com>
2025-01-31 17:07:08 +0200

    Minor changes

--- labs/lab1.1/csharp/Vector2.cs
+++ labs/lab1.1/csharp/Vector2.cs
@@ -3,7 +3,7 @@ namespace Program;
 class Vector2(double x = 0, double y = 0) {
     public double X {get;} = x;
     public double Y {get;} = y;
-    public double Magnitude {get;} = Math.Sqrt(x*x+y*y);
 
+    public double Magnitude() => Math.Sqrt(this.X*this.X + this.Y*this.Y);
     public override string ToString() => $"{this.X}, {this.Y}";
 }
